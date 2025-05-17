47c25ed17d9159aee943932490e6fd5596a78c15
Beingpax <prakashjoshipaxtek@gmail.com>
2025-05-17 19:06:51 +0545

    Add proper sizing for Milkdown editor

--- src/components/MarkdownEditor.tsx
+++ src/components/MarkdownEditor.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import { Crepe } from '@milkdown/crepe';
 import '@milkdown/crepe/theme/common/style.css';
 import '@milkdown/crepe/theme/frame.css';
+import '../styles/MilkdownEditor.css';
 
 interface MarkdownEditorProps {
   defaultValue?: string;
@@ -29,26 +30,6 @@ export default function MarkdownEditor({ defaultValue = '', onChange }: Markdown
     crepe.create().then(() => {
       console.log('Milkdown editor created');
       setEditorReady(true);
-      
-      // Apply styles to Milkdown elements to make them fill the container
-      if (editorRef.current) {
-        const milkdownElement = editorRef.current.querySelector('.milkdown');
-        if (milkdownElement instanceof HTMLElement) {
-          milkdownElement.style.width = '100%';
-          milkdownElement.style.height = '100%';
-          milkdownElement.style.boxSizing = 'border-box';
-          
-          // Find the editor content element and make it fill the available space
-          const editorElement = milkdownElement.querySelector('.editor');
-          if (editorElement instanceof HTMLElement) {
-            editorElement.style.width = '100%';
-            editorElement.style.height = '100%';
-            editorElement.style.maxWidth = '100%';
-            editorElement.style.padding = '0';
-            editorElement.style.margin = '0';
-          }
-        }
-      }
     });
 
     // Clean up
@@ -60,16 +41,5 @@ export default function MarkdownEditor({ defaultValue = '', onChange }: Markdown
     };
   }, [defaultValue, onChange]);
 
-  return (
-    <div 
-      ref={editorRef} 
-      style={{ 
-        height: '100%',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        overflow: 'auto'
-      }} 
-    />
-  );
+  return <div ref={editorRef} style={{ width: '100%', height: '100%' }} />;
 } 
