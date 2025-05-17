36de5c33eb1e163d3bbf84d7a65da50eaf4223f7
Beingpax <prakashjoshipaxtek@gmail.com>
2025-05-17 19:05:54 +0545

    Make Milkdown editor fill 100% of available space

--- src/components/MarkdownEditor.tsx
+++ src/components/MarkdownEditor.tsx
@@ -29,6 +29,26 @@ export default function MarkdownEditor({ defaultValue = '', onChange }: Markdown
     crepe.create().then(() => {
       console.log('Milkdown editor created');
       setEditorReady(true);
+      
+      // Apply styles to Milkdown elements to make them fill the container
+      if (editorRef.current) {
+        const milkdownElement = editorRef.current.querySelector('.milkdown');
+        if (milkdownElement instanceof HTMLElement) {
+          milkdownElement.style.width = '100%';
+          milkdownElement.style.height = '100%';
+          milkdownElement.style.boxSizing = 'border-box';
+          
+          // Find the editor content element and make it fill the available space
+          const editorElement = milkdownElement.querySelector('.editor');
+          if (editorElement instanceof HTMLElement) {
+            editorElement.style.width = '100%';
+            editorElement.style.height = '100%';
+            editorElement.style.maxWidth = '100%';
+            editorElement.style.padding = '0';
+            editorElement.style.margin = '0';
+          }
+        }
+      }
     });
 
     // Clean up
@@ -40,5 +60,16 @@ export default function MarkdownEditor({ defaultValue = '', onChange }: Markdown
     };
   }, [defaultValue, onChange]);
 
-  return <div ref={editorRef} style={{ height: '100%' }} />;
+  return (
+    <div 
+      ref={editorRef} 
+      style={{ 
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        overflow: 'auto'
+      }} 
+    />
+  );
 } 
